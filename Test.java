819c682bb41db6d94242ac28abfd3278e27f8aed
Elena Khatsko <khatskoelena@nixsolutions.com>
2017-10-13 16:40:32 +0300

    Test.java file

---
+++ Test.java
@@ -0,0 +1,16 @@
+package com.nixsolutions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test {
+public static void main(String [] srt) {
+	Map<String, String> hashmap = new HashMap<String, String>();
+	
+	hashmap.put("a", "b");
+	hashmap.put("a", "b");
+	System.out.println(hashmap.size());
+	
+	
+}
+}
