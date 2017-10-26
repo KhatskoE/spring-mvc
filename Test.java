5b9c766eb903361baa178515e9079900dca5e879
Elena Khatsko <khatskoelena@nixsolutions.com>
2017-10-26 16:40:32 +0300

    test

--- Test.java
+++ Test.java
@@ -7,7 +7,7 @@ public class Test {
 public static void main(String [] srt) {
 	Map<String, String> hashmap = new HashMap<String, String>();
 	
-	hashmap.put("a", "b");
+	hashmap.put("a", "b", "");
 	hashmap.put("a", "b");
 	System.out.println(hashmap.size());
 	
